9a015c273908d9b553a5c6e59bf11580d991fd90
bpuyol <148617619+bpuyol@users.noreply.github.com>
2024-01-30 17:53:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const palos = ["♠", "♥", "♣", "♦"];
+const valores = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function obtenerCartaAleatoria() {
+  const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return { palo: paloAleatorio, valor: valorAleatorio };
+}
+
+function generarCarta() {
+  const cartaAleatoria = obtenerCartaAleatoria();
+  const cartaElement = document.getElementById("cartaAleatoria");
+  cartaElement.innerHTML = `
+        <div class="esquina-superior-izquierda">${cartaAleatoria.palo}</div>
+        <div class="numero">${cartaAleatoria.valor}</div>
+        <div class="esquina-inferior-derecha">${cartaAleatoria.palo}</div>
+    `;
+}
+
+generarCarta();
